156d3453227323fa30d538b9f8e9f9eabe144f2f
kavalosc <59745971+kavalosc@users.noreply.github.com>
2023-03-09 10:21:05 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,6 +59,24 @@ export default function Home({ isLoading, children, setLoading }) {
           </button>
           );
         })}
+        <button className="pb-6">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-circle-plus stroke-edsightlightgray-100 hover:stroke-edsightyellow-400"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+              <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+              <path d="M9 12l6 0"></path>
+              <path d="M12 9l0 6"></path>
+            </svg>
+          </button>
       </ul>
 
           
